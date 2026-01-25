f7828158479fc9147d3672c77a3ad5634e9d1c2d
empathy-mp3 <mnemnth@icloud.com>
2026-01-24 18:52:54 -0700

    v1.2.1

--- Archipelago_Inscryption/ArchipelagoModPlugin.cs
+++ Archipelago_Inscryption/ArchipelagoModPlugin.cs
@@ -16,7 +16,7 @@ namespace Archipelago_Inscryption
     {
         internal const string PluginGuid = "ballininc.inscryption.archipelagomod";
         internal const string PluginName = "ArchipelagoMod";
-        internal const string PluginVersion = "1.2.0";
+        internal const string PluginVersion = "1.2.1";
 
         internal static ManualLogSource Log;
         internal static string SavePath => savePathConfig.Value;
