4ed80d0046e3fb83e68537ac46d60d75d99beffe
empathy-mp3 <mnemnth@icloud.com>
2026-02-02 19:30:51 -0700

    consumable checks

--- Archipelago_Inscryption/Patches/CheckPatches.cs
+++ Archipelago_Inscryption/Patches/CheckPatches.cs
@@ -1000,19 +1000,77 @@ namespace Archipelago_Inscryption.Patches
         {
             if (card.Info.name.Contains("ArchipelagoCheck"))
             {
-                if (__instance.peltCards[0].name == "Card (Rabbit Pelt)")
-                    ArchipelagoManager.SendCheck(APCheck.CabinTraderRabbitPelt);
-                else if (__instance.peltCards[0].name == "Card (Wolf Pelt)")
-                    ArchipelagoManager.SendCheck(APCheck.CabinTraderWolfPelt);
-                else
-                    ArchipelagoManager.SendCheck(APCheck.CabinTraderGoldenPelt);
-			    __instance.RemoveLastPelt();
 			    card.SetEnabled(false);
 			    __instance.tradeCards.Remove(card);
+                string cardName = card.Info.name;
+                string checkName = cardName.Substring(cardName.IndexOf('_') + 1);
+                APCheck check = Enum.GetValues(typeof(APCheck)).Cast<APCheck>().FirstOrDefault(c => c.ToString() == checkName);
+                ArchipelagoManager.SendCheck(check);
+				__instance.deckPile.MoveCardToPile(card, true, 0f, 0.7f);
                 __instance.deckPile.AddToPile(card.transform);
                 return false;
             }
             return true;
+        }
+
+        [HarmonyPatch(typeof(ItemSlot), "CreateItem")]
+        [HarmonyPostfix]
+        static void ReplaceSquirrelWithCheck(ItemSlot __instance)
+        {
+            if (__instance.Item is CardBottleItem && ArchipelagoOptions.randomizeChallenges != RandomizeChallenges.Disable
+                && Singleton<GameFlowManager>.Instance.CurrentGameState == GameState.SpecialCardSequence) {
+                int area = RunState.Run.regionTier;
+                List<APCheck> validChecks = [];
+                if (!ArchipelagoManager.HasCompletedCheck(APCheck.CabinWoodlandsConsumableCheck1 + area*3)){
+                    validChecks.Add(APCheck.CabinWoodlandsConsumableCheck1 + area*3);
+                }
+                if (!ArchipelagoManager.HasCompletedCheck(APCheck.CabinWoodlandsConsumableCheck2 + area*3)){
+                    validChecks.Add(APCheck.CabinWoodlandsConsumableCheck2 + area*3);
+                }
+                if (!ArchipelagoManager.HasCompletedCheck(APCheck.CabinWoodlandsConsumableCheck3 + area*3)){
+                    validChecks.Add(APCheck.CabinWoodlandsConsumableCheck3 + area*3);
+                }
+                if (validChecks.Count != 0)
+                {
+                    var bottle = __instance.Item as CardBottleItem;
+                    int seed = SaveManager.SaveFile.GetCurrentRandomSeed();
+                    seed += (int)(__instance.OriginalLocalPos.x + __instance.OriginalLocalPos.z) * 44;
+                    APCheck check = validChecks[SeededRandom.Range(0, validChecks.Count, seed++)];
+                    bottle.cardInfo = RandomizerHelper.GenerateCardInfo(check);
+                    var info = UnityEngine.Object.Instantiate(bottle.cardInfo);
+                    info.name = bottle.cardInfo.name;
+                    bottle.cardInfo = info;
+                    var card = bottle.GetComponentInChildren<SelectableCard>();
+                    var card2 = UnityEngine.Object.Instantiate(card);
+                    card2.name = card.name;
+                    card2.SetInfo(info);
+                    card2.transform.parent = card.transform.parent;
+                    card2.transform.localPosition = card.transform.localPosition;
+                    card2.transform.localRotation = card.transform.localRotation;
+                    card.gameObject.SetActive(false);
+                    card.transform.parent = null;
+                    UnityEngine.Object.Destroy(card);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(CardBottleItem), "ActivateSequence")]
+        [HarmonyPostfix]
+        static IEnumerator SendCheckInsteadOfAddingCard(IEnumerator __result, CardBottleItem __instance)
+        {
+            if (__instance.cardInfo.name.Contains("ArchipelagoCheck"))
+            {
+                __instance.PlayExitAnimation();
+                string cardName = __instance.cardInfo.name;
+                string checkName = cardName.Substring(cardName.IndexOf('_') + 1);
+                APCheck check = Enum.GetValues(typeof(APCheck)).Cast<APCheck>().FirstOrDefault(c => c.ToString() == checkName);
+                ArchipelagoManager.SendCheck(check);
+                yield return new WaitForSeconds(0.25f);
+            }
+            else {
+                while (__result.MoveNext())
+                    yield return __result.Current;
+            }
         }
     }
 
